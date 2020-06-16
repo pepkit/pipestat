34100fff173cc7fd1872b638c76b14f89aaa3ba6
Michal Stolarczyk <stolarczyk.michal93@gmail.com>
2020-06-15 21:40:36 -0400

    drop Python 3.5 support

--- setup.py
+++ setup.py
@@ -58,8 +58,6 @@ setup(
     classifiers=[
         "Development Status :: 4 - Beta",
         "License :: OSI Approved :: BSD License",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
