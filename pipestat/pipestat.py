5e33a1313d40716d729e210bd751cae341064eef
Michal Stolarczyk <stolarczyk.michal93@gmail.com>
2021-05-07 10:17:38 -0400

    always save results in memory in case of file backend

--- pipestat/pipestat.py
+++ pipestat/pipestat.py
@@ -203,6 +203,12 @@ class PipestatManager(dict):
             self.config_path,
         )
         if results_file_path:
+            if self[DB_ONLY_KEY]:
+                _LOGGER.warning(
+                    "Running in database only mode does not make sense with a YAML file as a backend. "
+                    "Changing back to using memory."
+                )
+                self[DB_ONLY_KEY] = not self[DB_ONLY_KEY]
             self[FILE_KEY] = results_file_path
             self._init_results_file()
             flag_file_dir = _select_value(
