1d3e65ceaf2d6d552698864b82d0da7c6f9ba72d
Michal Stolarczyk <stolarczyk.michal93@gmail.com>
2021-06-04 15:08:05 -0400

    expect another exception in tests

--- tests/test_db_only_mode.py
+++ tests/test_db_only_mode.py
@@ -13,7 +13,12 @@ class TestDatabaseOnly:
             {"percentage_of_things": 10.1},
         ],
     )
-    def test_report(self, val, config_file_path, schema_file_path, results_file_path):
+    def test_report(
+        self,
+        val,
+        config_file_path,
+        schema_file_path,
+    ):
         REC_ID = "constant_record_id"
         psm = PipestatManager(
             schema_path=schema_file_path,
@@ -28,15 +33,12 @@ class TestDatabaseOnly:
         assert psm.select(filter_conditions=[(val_name, "eq", str(val[val_name]))])
 
     @pytest.mark.parametrize(["rec_id", "res_id"], [("sample2", "number_of_things")])
-    @pytest.mark.parametrize("backend", ["db"])
     def test_select_invalid_filter_column(
         self,
         rec_id,
         res_id,
         config_file_path,
-        results_file_path,
         schema_file_path,
-        backend,
     ):
         args = dict(
             schema_path=schema_file_path, namespace="test", config=config_file_path
@@ -48,40 +50,33 @@ class TestDatabaseOnly:
                 columns=[res_id],
             )
 
-    @pytest.mark.parametrize(["rec_id", "res_id"], [("sample2", "number_of_things")])
-    @pytest.mark.parametrize("backend", ["db"])
+    @pytest.mark.parametrize("res_id", ["number_of_things"])
     @pytest.mark.parametrize("filter", [("column", "eq", 1), "a", [1, 2, 3]])
     def test_select_invalid_filter_structure(
         self,
-        rec_id,
         res_id,
         config_file_path,
-        results_file_path,
         schema_file_path,
-        backend,
         filter,
     ):
         args = dict(
             schema_path=schema_file_path, namespace="test", config=config_file_path
         )
         psm = PipestatManager(**args)
-        with pytest.raises(ValueError):
+        with pytest.raises((ValueError, TypeError)):
             psm.select(
                 filter_conditions=[filter],
                 columns=[res_id],
             )
 
     @pytest.mark.parametrize(["rec_id", "res_id"], [("sample2", "number_of_things")])
-    @pytest.mark.parametrize("backend", ["db"])
     @pytest.mark.parametrize("limit", [1, 2, 3, 15555])
     def test_select_limit(
         self,
         rec_id,
         res_id,
         config_file_path,
-        results_file_path,
         schema_file_path,
-        backend,
         limit,
     ):
         args = dict(
@@ -95,10 +90,12 @@ class TestDatabaseOnly:
         )
         assert len(result) <= limit
 
-    @pytest.mark.parametrize("backend", ["db"])
     @pytest.mark.parametrize("offset", [0, 1, 2, 3, 15555])
     def test_select_offset(
-        self, config_file_path, results_file_path, schema_file_path, backend, offset
+        self,
+        config_file_path,
+        schema_file_path,
+        offset,
     ):
         args = dict(
             schema_path=schema_file_path, namespace="test", config=config_file_path
@@ -108,16 +105,13 @@ class TestDatabaseOnly:
         print(result)
         assert len(result) == max((psm.record_count - offset), 0)
 
-    @pytest.mark.parametrize("backend", ["db"])
     @pytest.mark.parametrize(
         ["offset", "limit"], [(0, 0), (0, 1), (0, 2), (0, 11111), (1, 1), (1, 0)]
     )
     def test_select_pagination(
         self,
         config_file_path,
-        results_file_path,
         schema_file_path,
-        backend,
         offset,
         limit,
     ):
