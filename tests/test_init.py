6e0ff111b694bf64f3e3e08c0284386c1348cca4
Vince Reuter <vince.reuter@gmail.com>
2023-04-12 14:26:59 +0200

    remove connection tests

--- tests/test_init.py
+++ tests/test_init.py
@@ -9,33 +9,11 @@ from pipestat.const import *
 from pipestat.exceptions import *
 
 
-class TestConnection:
-    def test_connection_checker(self, config_file_path, schema_file_path):
-        pm = PipestatManager(
-            config=config_file_path,
-            database_only=True,
-            schema_path=schema_file_path,
-            namespace="test",
-        )
-        assert pm.is_db_connected()
-
-    def test_connection_overwrite_error(self, config_file_path, schema_file_path):
-        pm = PipestatManager(
-            config=config_file_path,
-            database_only=True,
-            schema_path=schema_file_path,
-            namespace="test",
-        )
-        with pytest.raises(PipestatDatabaseError):
-            pm.establish_db_connection()
-
-
 class TestPipestatManagerInstantiation:
     def test_obj_creation_file(self, schema_file_path, results_file_path):
         """Object constructor works with file as backend"""
         assert isinstance(
             PipestatManager(
-                namespace="test",
                 results_file_path=results_file_path,
                 schema_path=schema_file_path,
             ),
@@ -132,14 +110,11 @@ class TestPipestatManagerInstantiation:
     def test_wrong_class_results_file(self, schema_file_path, pth):
         """Input string that is not a file path raises an informative error"""
         with pytest.raises((TypeError, AssertionError)):
-            PipestatManager(
-                namespace="test", results_file_path=pth, schema_path=schema_file_path
-            )
+            PipestatManager(results_file_path=pth, schema_path=schema_file_path)
 
     def test_results_file_contents_loaded(self, results_file_path, schema_file_path):
         """Contents of the results file are present after loading"""
         psm = PipestatManager(
-            namespace="test",
             results_file_path=results_file_path,
             schema_path=schema_file_path,
         )
@@ -147,9 +122,5 @@ class TestPipestatManagerInstantiation:
 
     def test_str_representation(self, results_file_path, schema_file_path):
         """Test string representation identifies number of records"""
-        psm = PipestatManager(
-            namespace="test",
-            results_file_path=results_file_path,
-            schema_path=schema_file_path,
-        )
+        psm = PipestatManager(results_file_path=results_file_path, schema_path=schema_file_path)
         assert f"Records count: {len(psm.data[psm.namespace])}" in str(psm)
