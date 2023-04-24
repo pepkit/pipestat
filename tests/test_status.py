1783e3ec98eaac936b05e2ffc4c157ee6132e363
Vince Reuter <vince.reuter@gmail.com>
2023-04-24 23:46:38 +0200

    better organization and syntax in status test suite

--- tests/test_status.py
+++ tests/test_status.py
@@ -11,11 +11,11 @@ BACKEND_KEY_FILE = "file"
 
 @pytest.fixture
 def backend_data(request, config_file_path, results_file_path):
-    return (
-        {"config": config_file_path}
-        if request.param == "db"
-        else {"results_file_path": results_file_path}
-    )
+    if request.param == BACKEND_KEY_DB:
+        return {"config": config_file_path}
+    elif request.param == BACKEND_KEY_FILE:
+        return {"results_file_path": results_file_path}
+    raise Exception(f"Unrecognized initial parametrization for backend data: {request.param}")
 
 
 def test_status_file_default_location(schema_file_path, results_file_path):
@@ -27,24 +27,21 @@ def test_status_file_default_location(schema_file_path, results_file_path):
     )
     assert psm[STATUS_FILE_DIR] == os.path.dirname(psm.file)
 
-@pytest.mark.parametrize("backend", ["file", "db"])
+
+@pytest.mark.parametrize("backend_data", ["file", "db"], indirect=True)
 @pytest.mark.parametrize("status_id", ["running", "failed", "completed"])
 def test_status_not_configured(
-    schema_file_path, config_file_path, results_file_path, backend, status_id
+    schema_file_path, config_file_path, backend_data, status_id
 ):
     """Status management works even in case it has not been configured."""
     args = dict(
         schema_path=schema_file_path,
     )
-    backend_data = (
-        {"config": config_file_path}
-        if backend == "db"
-        else {"results_file_path": results_file_path}
-    )
     args.update(backend_data)
     psm = PipestatManager(**args)
     psm.set_status(record_identifier="sample1", status_identifier=status_id)
     assert psm.get_status(record_identifier="sample1") == status_id
+
 
 @pytest.mark.parametrize(
     "backend_data", [BACKEND_KEY_FILE, BACKEND_KEY_DB], indirect=True
