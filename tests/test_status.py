6a5a76d85e311d57d75b10f4b776737d957d5d7f
Vince Reuter <vince.reuter@gmail.com>
2023-04-24 23:41:45 +0200

    remove unnecessary test nesting

--- tests/test_status.py
+++ tests/test_status.py
@@ -18,52 +18,51 @@ def backend_data(request, config_file_path, results_file_path):
     )
 
 
-class TestStatus:
-    def test_status_file_default_location(self, schema_file_path, results_file_path):
-        """status file location is set to the results file dir
-        if not specified"""
-        psm = PipestatManager(
-            results_file_path=results_file_path,
-            schema_path=schema_file_path,
-        )
-        assert psm[STATUS_FILE_DIR] == os.path.dirname(psm.file)
-
-    @pytest.mark.parametrize("backend", ["file", "db"])
-    @pytest.mark.parametrize("status_id", ["running", "failed", "completed"])
-    def test_status_not_configured(
-        self, schema_file_path, config_file_path, results_file_path, backend, status_id
-    ):
-        """Status management works even in case it has not been configured."""
-        args = dict(
-            schema_path=schema_file_path,
-        )
-        backend_data = (
-            {"config": config_file_path}
-            if backend == "db"
-            else {"results_file_path": results_file_path}
-        )
-        args.update(backend_data)
-        psm = PipestatManager(**args)
-        psm.set_status(record_identifier="sample1", status_identifier=status_id)
-        assert psm.get_status(record_identifier="sample1") == status_id
-
-    @pytest.mark.parametrize(
-        "backend_data", [BACKEND_KEY_FILE, BACKEND_KEY_DB], indirect=True
+def test_status_file_default_location(self, schema_file_path, results_file_path):
+    """status file location is set to the results file dir
+    if not specified"""
+    psm = PipestatManager(
+        results_file_path=results_file_path,
+        schema_path=schema_file_path,
     )
-    @pytest.mark.parametrize(
-        "status_id", ["running_custom", "failed_custom", "completed_custom"]
+    assert psm[STATUS_FILE_DIR] == os.path.dirname(psm.file)
+
+@pytest.mark.parametrize("backend", ["file", "db"])
+@pytest.mark.parametrize("status_id", ["running", "failed", "completed"])
+def test_status_not_configured(
+    self, schema_file_path, config_file_path, results_file_path, backend, status_id
+):
+    """Status management works even in case it has not been configured."""
+    args = dict(
+        schema_path=schema_file_path,
     )
-    def test_custom_status_schema(
-        self,
-        backend_data,
-        status_id,
-        custom_status_schema,
-    ):
-        """Status management works even in case it has not been configured."""
-        args = dict(
-            schema_path=custom_status_schema,
-        )
-        args.update(backend_data)
-        psm = PipestatManager(**args)
-        psm.set_status(record_identifier="sample1", status_identifier=status_id)
-        assert psm.get_status(record_identifier="sample1") == status_id
+    backend_data = (
+        {"config": config_file_path}
+        if backend == "db"
+        else {"results_file_path": results_file_path}
+    )
+    args.update(backend_data)
+    psm = PipestatManager(**args)
+    psm.set_status(record_identifier="sample1", status_identifier=status_id)
+    assert psm.get_status(record_identifier="sample1") == status_id
+
+@pytest.mark.parametrize(
+    "backend_data", [BACKEND_KEY_FILE, BACKEND_KEY_DB], indirect=True
+)
+@pytest.mark.parametrize(
+    "status_id", ["running_custom", "failed_custom", "completed_custom"]
+)
+def test_custom_status_schema(
+    self,
+    backend_data,
+    status_id,
+    custom_status_schema,
+):
+    """Status management works even in case it has not been configured."""
+    args = dict(
+        schema_path=custom_status_schema,
+    )
+    args.update(backend_data)
+    psm = PipestatManager(**args)
+    psm.set_status(record_identifier="sample1", status_identifier=status_id)
+    assert psm.get_status(record_identifier="sample1") == status_id
