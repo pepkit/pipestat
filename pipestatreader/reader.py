d3001cbe38f9baa50d29b87bcb135ad687ceda0c
Donald Campbell <125581724+donaldcampbelljr@users.noreply.github.com>
2023-09-15 17:34:30 -0400

    add __init__.py and main func

--- pipestatreader/reader.py
+++ pipestatreader/reader.py
@@ -14,12 +14,14 @@ app = fastapi.FastAPI(
     version="0.01",
 )
 
+
 @app.get("/")
 async def home():
     """
     Display the home page
     """
-    return {"Home":"Welcome"}
+    return {"Home": "Welcome"}
+
 
 @app.get("/data/{record_identifier}")
 async def retrieve_results(record_identifier: str):
@@ -32,17 +34,21 @@ async def retrieve_results(record_identifier: str):
         return {"result": "Record not found"}
     return {"result": result}
 
+
 @app.get("/data/{record_identifier}/{result_identifier}")
 async def retrieve_results(record_identifier: str, result_identifier: str):
     """
     Get specific result given a record identifier and a result identifier
     """
     try:
-        result = psm.retrieve(record_identifier=record_identifier, result_identifier=result_identifier)
+        result = psm.retrieve(
+            record_identifier=record_identifier, result_identifier=result_identifier
+        )
     except RecordNotFoundError:
         # TODO this should be more specific than record not found because maybe it's just the result that does not exist
         return {"result": "Record not found"}
     return {"result": result}
+
 
 @app.get("/output_schema/")
 async def retrieve_output_schema(pipeline_type: Optional[str] = None):
@@ -51,28 +57,18 @@ async def retrieve_output_schema(pipeline_type: Optional[str] = None):
     """
 
     if pipeline_type == "sample":
-        return {"output schema":psm.schema._sample_level_data}
+        return {"output schema": psm.schema._sample_level_data}
     if pipeline_type == "project":
-        return {"output schema":psm.schema._project_level_data}
+        return {"output schema": psm.schema._project_level_data}
     if pipeline_type is None:
         return {"output schema": psm.schema}
     else:
         return {"output schema": "output schema not found"}
 
-# def pipestat_reader(db_config):
-#     print("hello from reader")
-#     # Create a FastAPI instance
-#     app = fastapi.FastAPI(
-#     title="PIPESTATREADER",
-#     description="Allows reading pipestat files from a POSTGRESQL Server",
-#     version="0.01",
-#     )
-#     #uvicorn.run("app:app", host='0.0.0.0', port=8000, reload=True, workers=3)
-#     #uvicorn.run("app:app", host='127.0.0.1', port=5432, reload=True, workers=3)
-#     print(db_config)
-#     @app.get("/items/{item_id}")
-#     async def read_item(item_id):
-#         return {"item_id": item_id}
+
+def main():
+    uvicorn.run("reader:app", host="0.0.0.0", port=8000, reload=True)
+
 
 if __name__ == "__main__":
-    uvicorn.run("reader:app", host="0.0.0.0", port=8000, reload=True)
+    main()
