aac6fbf2c11ec17a6325e0160a2cc565ed0c191b
Donald Campbell <125581724+donaldcampbelljr@users.noreply.github.com>
2023-09-19 12:27:00 -0400

    update endpoints, add Query

--- pipestatreader/reader.py
+++ pipestatreader/reader.py
@@ -2,7 +2,7 @@ import fastapi
 import uvicorn
 from typing import Optional
 from pipestat import RecordNotFoundError, SamplePipestatManager
-
+from fastapi import Query
 
 # For testing, simply hardcode config atm
 pipestatcfg = "/home/drc/PythonProjects/pipestat/testimport/drcdbconfig.yaml"
@@ -66,7 +66,7 @@ async def retrieve_output_schema(pipeline_type: Optional[str] = None):
         return {"output schema": "output schema not found"}
 
 
-@app.get("/table_contents/")
+@app.get("/all_table_contents/")
 async def retrieve_table_contents():
     """
     Get all table contents
@@ -75,18 +75,66 @@ async def retrieve_table_contents():
     results = psm.backend.select()
     return {"table_contents": results}
 
+@app.get("/images/")
+async def retrieve_images():
+    """
+    Get all image paths reported in the table.
+    """
+    # Add skip and limit here as well.
+    #results = psm.backend.select()
+    # Should be able to determine if result schema has image type and then use select function to retrieve those
+    # check result schema first?
+    return {"image_paths": "Not implemented."}
 
-@app.get("/table_contents/{column_name}")
-async def retrieve_column_contents(column_name: Optional[str] = None):
+@app.get("/files/")
+async def retrieve_images():
+    """
+    Get all image paths reported in the table.
+    """
+    # Add skip and limit here as well.
+    # results = psm.backend.select()
+    # Should be able to determine if result schema has file type and then use select function to retrieve those
+    # check result schema first?
+    return {"file_paths": "Not implemented."}
+
+@app.get("/filtered_table_contents/")
+async def retrieve_filtered_table_contents(
+        column_name_list: Optional[list[str]] = Query(default=None, description="List of column names to select from the table"),
+        #filter_conditions: Optional[list[tuple[str,str,str]]] = Query(None),
+                                   ):
     """
     Get column contents for a specific column name
     """
     # Add skip and limit here as well.
-    results = psm.backend.select(columns=[column_name])
+    try:
+        results = psm.backend.select(columns=column_name_list, filter_conditions=None)
+    except AttributeError:
+        return {"column_contents": f"Attribute error for query: {column_name_list}"}
     if results is not None:
         return {"column_contents": results}
     else:
         return {"column_contents": "This column does not exist."}
+
+# @app.get("/filtered_table_contents/")
+# async def retrieve_filtered_column_contents(column_name: Optional[str] = None, filter_conditions: Optional[tuple[str,str,str]] = None):
+#     """
+#     Filter table contents based on column names and/or filter conditions
+#     """
+#     # Add skip and limit here as well.
+#     # filter_conditions=[("record_identifier", "eq", "random_sample_i2")])
+#     if column_name is None and filter_conditions is None:
+#         return {"filtered_table_results": psm.backend.select()}
+#     if column_name and filter_conditions:
+#         results = psm.backend.select(columns=[column_name], filter_conditions=[filter_conditions])
+#     if column_name:
+#         results = psm.backend.select(columns=[column_name])
+#     if filter_conditions:
+#         filter_conditions = tuple(filter_conditions)
+#         results = psm.backend.select(filter_conditions=filter_conditions)
+#     if results is not None:
+#         return {"column_contents": results}
+#     else:
+#         return {"column_contents": "not found"}
 
 
 def main():
