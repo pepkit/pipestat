b2fb9fa8a3d786da99a9947e8b28633ac0ef4cda
Donald Campbell <125581724+donaldcampbelljr@users.noreply.github.com>
2023-10-25 09:52:44 -0400

    polishing decorator for checking dependencies

--- pipestat/pipestat.py
+++ pipestat/pipestat.py
@@ -13,7 +13,7 @@ try:
     from pipestat.backends.db_backend.dbbackend import DBBackend
     from pipestat.backends.db_backend.db_helpers import *
 except ImportError:
-    # We let this pass, but if the user attempts to create DBBackend, check_db_dependencies raises exception.
+    # We let this pass, but if the user attempts to create DBBackend, check_dependencies raises exception.
     pass
 
 try:
@@ -34,24 +34,24 @@ from .reports import HTMLReportBuilder, _create_stats_objs_summaries
 _LOGGER = getLogger(PKG_NAME)
 
 
-def check_dependency_list(dependency_list):
-    """Function to check that the DB Backend has successfully been imported."""
+def check_dependency_list(dependency_list: list = None):
+    """Function to check that the dependency list has successfully been imported."""
     dependencies_satisfied = True
 
     if dependency_list is not None:
         for i in dependency_list:
             if i not in globals():
-                _LOGGER.warning(msg="Need db dependencies")
+                _LOGGER.warning(msg=f"Missing dependency: {i}")
                 dependencies_satisfied = False
 
     if dependencies_satisfied is False:
         raise PipestatDependencyError(
-            msg="Missing required dependencies for this usage. Try pip install pipestat['db-backend']"
+            msg="Missing required dependencies for this usage, e.g. try pip install pipestat['db-backend']"
         )
 
 
-def check_dependencies(dependency_list):
-    """Function to check that the DB Backend has successfully been imported."""
+def check_dependencies(dependency_list: list = None):
+    """Decorator to check that the dependency list has successfully been imported."""
 
     def wrapper(func):
         def inner(*args, **kwargs):
